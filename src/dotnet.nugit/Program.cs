c36312173f4c32c2674d675dc7c5f02b52121c79
Matthias Friedrich <1573457+matzefriedrich@users.noreply.github.com>
2024-04-04 23:38:58 +0200

    Adds an example sub-command

--- src/dotnet.nugit/Program.cs
+++ src/dotnet.nugit/Program.cs
@@ -1,9 +1,26 @@
-﻿namespace dotnet.nugit;
+﻿using System.CommandLine;
+using System.CommandLine.Extensions;
+
+namespace dotnet.nugit;
 
 internal static class Program
 {
-    public static void Main(string[] args)
+    public static int Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        var app = new CommandLineApplication();
+
+        app.Command("greeting", "Greets the specified person.", greeting =>
+        {
+            greeting.Option<string>("--name", "The person´s name.", ArgumentArity.ExactlyOne)
+                .Option<bool>("--polite")
+                .OnExecute(async (string name, bool polite) =>
+                {
+                    if (polite) Console.WriteLine($"Good day {name}");
+                    else Console.WriteLine($"Hello {name}");
+                    return await Task.FromResult(0);
+                });
+        });
+
+        return app.Execute(args);
     }
 }
